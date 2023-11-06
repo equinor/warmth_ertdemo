ed0e51e2b9228845d9f7790e1f6b04f384d326d7
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2023-11-06 12:46:53 +0000

    DOC: dev container

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,2 @@
+poetry install --with dev --no-interaction
+poetry run pip install petsc4py==3.20.0 mpi4py==3.1.5 https://github.com/FEniCS/ufl/archive/refs/tags/2023.2.0.zip https://github.com/FEniCS/ffcx/archive/v0.7.0.zip https://github.com/FEniCS/basix/archive/v0.7.0.zip
